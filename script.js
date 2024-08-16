dc6c56bc9213521ae9edcc68d965c1f86fb91d86
Bigwills <ajbodesecond@gmail.com>
2024-08-16 16:30:21 +0100

    Allow multiple serial codes

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const textarea = document.querySelector("#textarea");
 const resultCtn = document.querySelector(".result-ctn");
 const cover = document.querySelector(".cover");
 const closeBtn = document.querySelector(".close-btn");
-const serialNum = document.querySelector(".serial-num");
+const serialNumcorrt = document.querySelector(".serial-num-correct");
+const serialNumwrong = document.querySelector(".serial-num-wrong");
 const username = document.querySelector("#name");
 const number = document.querySelector("#number");
 const date = document.querySelector("#date");
@@ -12,26 +13,33 @@ const resultPassCtn = document.querySelector(".result-pass-ctn");
 const proceedBtn = document.querySelector(".proceed-btn");
 const form = document.querySelector(".form");
 
-const correctSerial = "ML7RXYJ4CJ";
+// const correctSerial = "ML7RXYJ4CJ";
+const correctSerial = ["MGXBBMGX", "MGXCQRKDPF"];
 submitBtn.addEventListener("click", () => {
-  let typed = textarea.value;
+  let typed = textarea.value.toUpperCase().replace(/\s+/g, ""); // Convert to uppercase
 
-  if (typed.toUpperCase() === correctSerial) {
-    cover.style.display = "flex";
-    resultCtn.style.display = "none";
-    resultPassCtn.style.display = "flex";
-  } else {
+  // Flag for successful match
+  let isMatchFound = false;
+
+  correctSerial.forEach((serialNumber) => {
+    if (typed === serialNumber) {
+      cover.style.display = "flex";
+      resultCtn.style.display = "none";
+      resultPassCtn.style.display = "flex";
+      isMatchFound = true;
+      return;
+    }
+  });
+
+  serialNumcorrt.textContent = isMatchFound ? typed : ""; // Empty for no match
+  serialNumwrong.textContent = !isMatchFound && typed !== "" ? typed : ""; // Empty for empty field or match
+
+  if (typed === "" || !isMatchFound) {
     cover.style.display = "flex";
     resultCtn.style.display = "flex";
     resultPassCtn.style.display = "none";
-    serialNum.textContent = textarea.value;
   }
-  if (typed === "") {
-    cover.style.display = "none";
-    resultCtn.style.display = "none";
-    resultPassCtn.style.display = "none";
-    alert("Serial Number Field is empty!");
-  }
+
   if (username.value == "") {
     cover.style.display = "none";
     resultCtn.style.display = "none";
